af71806df5df92c6062f074d873cb7eae6898acd
olexan7 <ghoostt61@gmail.com>
2020-02-28 00:41:53 +0400

    feat: complete task Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  var reverseArr = n.toString().split("").reverse();
+  if(reverseArr[0]==0) reverseArr.shift();
+  if(reverseArr[reverseArr.length-1] == "-") reverseArr.pop();
+  return reverseArr.join("");
 }
